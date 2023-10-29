2db99b4e92560954194ce50ce4ec4b042fbcbc00
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-29 12:11:34 +0300

    2-я домашка с массивами

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
     }
-    // Задача 1
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+            }
+        return arr;
+    }
+    // Задача № 1
+        public static void task1() {
+            int[] arr = generateRandomArray();
+            int i = 0;
+            int summ = arr[i];
+            for (; i <= arr.length; i++) {
+                summ ++;
+            }
+            System.out.println(summ);
+        }
+
 }
