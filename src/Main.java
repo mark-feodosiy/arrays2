3aef6e4d01ba48ad87bf00a8f2dead70c5bb21c3
mark-feodosiy <mark-feodosiy@yandex.ru>
2023-10-30 12:18:02 +0300

    2-я домашка с массивами

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,54 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
         System.out.println();
     }
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
-        int[] arr = new int[10];
+        int[] arr = new int[30];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = random.nextInt(100_000) + 100_000;
             }
         return arr;
     }
-        // Задача № 1
+    // Задача № 1
         public static void task1() {
             int[] arr = generateRandomArray();
-            System.out.println(Arrays.toString(arr));
+            //System.out.println(Arrays.toString(arr));
             int sum = 0;
             for (int element: arr) {
                 sum += element;
             }
             System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+            System.out.println();
         }
     // Задача № 2
     public static void task2() {
         int[] arr = generateRandomArray();
-        System.out.println(Arrays.toString(arr));
-        int minWastes = 0;
-        for (int element: arr) {
-            if (element < element + 1) {
-                minWastes = element;
+        //System.out.println(Arrays.toString(arr));
+        int minWastes = arr[0];
+        int maxWastes = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < minWastes) {
+                minWastes = arr[i];
+            }
+            if (arr[i] > maxWastes) {
+                maxWastes = arr[i];
             }
         }
-        System.out.println("Минимальная сумма трат за день составила " + minWastes + " рублей");
+        System.out.println("Минимальная сумма трат за день составила " + minWastes + " рублей. Максимальная сумма трат за день составила " + maxWastes + " рублей");
+        System.out.println();
     }
-}
+        // Задача № 3
+        public static void task3() {
+            int[] arr = generateRandomArray();
+            //System.out.println(Arrays.toString(arr));
+            double sum = 0;
+            for (int element: arr) {
+                sum += element;
+            }
+            double average = sum/ 30;
+            System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+        }
+    }
